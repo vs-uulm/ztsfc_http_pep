c4865cf92ef0d42a32d4fb9bff011b59e0b08191
Marie Lohbeck <marie.lohbeck@uni-ulm.de>
2021-06-23 21:41:33 +0200

    moved ldap config to config file

--- basic_auth/basic_auth.go
+++ basic_auth/basic_auth.go
@@ -1,219 +1,222 @@
 package basic_auth
 
 import (
-    "net/http"
-    //"net/http/httputil"
-    //"net/url"
-    "fmt"
-    //"crypto/tls"
-    "github.com/dgrijalva/jwt-go"
-    "crypto/rsa"
-    "crypto/x509"
-    "encoding/pem"
-    "io/ioutil"
-    "time"
-    "github.com/jtblin/go-ldap-client"
-    //env "local.com/leobrada/ztsfc_http_pep/env"
-    metadata "local.com/leobrada/ztsfc_http_pep/metadata"
-//    proxies "local.com/leobrada/ztsfc_http_pep/proxies"
+	"net/http"
+	//"net/http/httputil"
+	//"net/url"
+	"fmt"
+	//"crypto/tls"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/jtblin/go-ldap-client"
+
+	//env "local.com/leobrada/ztsfc_http_pep/env"
+	"local.com/leobrada/ztsfc_http_pep/env"
+	metadata "local.com/leobrada/ztsfc_http_pep/metadata"
+	//    proxies "local.com/leobrada/ztsfc_http_pep/proxies"
 )
 
 var (
-    Jwt_pub_key *rsa.PublicKey
-    MySigningKey *rsa.PrivateKey
+	Jwt_pub_key  *rsa.PublicKey
+	MySigningKey *rsa.PrivateKey
+
 //    Jwt_priv_key *rsa.*rsa.PrivateKey
 )
 
 func User_sessions_is_valid(req *http.Request, cpm *metadata.Cp_metadata) bool {
-    jwt_cookie, err := req.Cookie("ztsfc_session")
-    if err != nil {
-        return false
-    }
-    ss := jwt_cookie.Value
+	jwt_cookie, err := req.Cookie("ztsfc_session")
+	if err != nil {
+		return false
+	}
+	ss := jwt_cookie.Value
 
-    token, err := jwt.Parse(ss , func(token *jwt.Token) (interface{}, error) {
-        return Jwt_pub_key, nil
-    })
+	token, err := jwt.Parse(ss, func(token *jwt.Token) (interface{}, error) {
+		return Jwt_pub_key, nil
+	})
 
-    if err != nil {
-        return false
-    }
+	if err != nil {
+		return false
+	}
 
-    username := token.Claims.(jwt.MapClaims)["sub"].(string)
-    cpm.User = username
-    cpm.Pw_authenticated = true
-    cpm.Cert_authenticated = perform_x509_auth(req)
+	username := token.Claims.(jwt.MapClaims)["sub"].(string)
+	cpm.User = username
+	cpm.Pw_authenticated = true
+	cpm.Cert_authenticated = perform_x509_auth(req)
 
-    return true
+	return true
 }
 
 func Basic_auth(w http.ResponseWriter, req *http.Request) bool {
 
-//    proxies.Basic_auth_proxy.ServeHTTP(w, req)
-    if perform_passwd_auth(w, req) {
-        return true
-    }
+	//    proxies.Basic_auth_proxy.ServeHTTP(w, req)
+	if perform_passwd_auth(w, req) {
+		return true
+	}
 
-    return false
+	return false
 }
 
 func perform_passwd_auth(w http.ResponseWriter, req *http.Request) bool {
-    var username, password string
+	var username, password string
 
-    // TODO: Check for JW Token initially
-    // Check if it is a POST request
-    if req.Method =="POST" {
+	// TODO: Check for JW Token initially
+	// Check if it is a POST request
+	if req.Method == "POST" {
 
-        if err := req.ParseForm(); err != nil {
-            handleFormReponse("Parsing Error", w)
-            return false
-        }
+		if err := req.ParseForm(); err != nil {
+			handleFormReponse("Parsing Error", w)
+			return false
+		}
 
-        nmbr_of_postvalues := len(req.PostForm)
-        if nmbr_of_postvalues != 2 {
-            handleFormReponse("Wrong number of POST form values", w)
-            return false
-        }
+		nmbr_of_postvalues := len(req.PostForm)
+		if nmbr_of_postvalues != 2 {
+			handleFormReponse("Wrong number of POST form values", w)
+			return false
+		}
 
-        usernamel, exist := req.PostForm["username"]
-        username = usernamel[0]
-        if !exist {
-            handleFormReponse("Username not present or wrong", w)
-            return false
-        }
+		usernamel, exist := req.PostForm["username"]
+		username = usernamel[0]
+		if !exist {
+			handleFormReponse("Username not present or wrong", w)
+			return false
+		}
 
-        passwordl, exist := req.PostForm["password"]
-        password = passwordl[0]
-        if !exist {
-            handleFormReponse("Password not present or wrong", w)
-            return false
-        }
+		passwordl, exist := req.PostForm["password"]
+		password = passwordl[0]
+		if !exist {
+			handleFormReponse("Password not present or wrong", w)
+			return false
+		}
 
-        if !userIsInLDAP(username, password) {
-            handleFormReponse("Authentication failed for user", w)
-            return false
-        }
+		if !userIsInLDAP(username, password) {
+			handleFormReponse("Authentication failed for user", w)
+			return false
+		}
 
-        // Create JWT
-        //MySigningKey := parseRsaiPrivateKeyFromPemStr("./basic_auth/jwt_test_priv.pem")
-        ss := createJWToken(MySigningKey, username)
-        //fmt.Println(ss)
+		// Create JWT
+		//MySigningKey := parseRsaiPrivateKeyFromPemStr("./basic_auth/jwt_test_priv.pem")
+		ss := createJWToken(MySigningKey, username)
+		//fmt.Println(ss)
 
-        ztsfc_cookie := http.Cookie{
-            Name: "ztsfc_session",
-            Value: ss,
-            MaxAge: 1800,
-            Path: "/",
-        }
-        http.SetCookie(w, &ztsfc_cookie)
+		ztsfc_cookie := http.Cookie{
+			Name:   "ztsfc_session",
+			Value:  ss,
+			MaxAge: 1800,
+			Path:   "/",
+		}
+		http.SetCookie(w, &ztsfc_cookie)
 
-        // TODO: make it user configurable
-        // TODO: is there a better solution for the content-length  /body length "bug"?
-        req.ContentLength = 0
-        http.Redirect(w, req, "https://" + req.Host + req.URL.String(), 303)
-        return false
+		// TODO: make it user configurable
+		// TODO: is there a better solution for the content-length  /body length "bug"?
+		req.ContentLength = 0
+		http.Redirect(w, req, "https://"+req.Host+req.URL.String(), 303)
+		return false
 
-    } else {
-        handleFormReponse("only post methods are accepted in this state", w)
-        return false
-    }
+	} else {
+		handleFormReponse("only post methods are accepted in this state", w)
+		return false
+	}
 }
 
-func createJWToken(mySigningKey *rsa.PrivateKey, username string) (string) {
-    claims := &jwt.StandardClaims{
-        ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
-        Issuer: "ztsfc_bauth",
-        Subject: username,
-    }
+func createJWToken(mySigningKey *rsa.PrivateKey, username string) string {
+	claims := &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
+		Issuer:    "ztsfc_bauth",
+		Subject:   username,
+	}
 
-    token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-    ss, _ := token.SignedString(mySigningKey)
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	ss, _ := token.SignedString(mySigningKey)
 
-//    fmt.Printf("%v\n", ss)
-    return ss
+	//    fmt.Printf("%v\n", ss)
+	return ss
 }
 
 func perform_x509_auth(req *http.Request) bool {
-   // Check if a verified client certificate is present
-    if len(req.TLS.VerifiedChains) > 0 && req.TLS.ServerName == "service1.testbed.informatik.uni-ulm.de"{
-        return true
-    }
+	// Check if a verified client certificate is present
+	if len(req.TLS.VerifiedChains) > 0 && req.TLS.ServerName == "service1.testbed.informatik.uni-ulm.de" {
+		return true
+	}
 
-    return false
+	return false
 }
 
-func ParseRsaPublicKeyFromPemStr(pubPEMlocation string) (*rsa.PublicKey) {
-    pub_read_in, err := ioutil.ReadFile(pubPEMlocation)
-    if err != nil {
-        fmt.Printf("Could not read from file.\n")
-        return nil
-    }
+func ParseRsaPublicKeyFromPemStr(pubPEMlocation string) *rsa.PublicKey {
+	pub_read_in, err := ioutil.ReadFile(pubPEMlocation)
+	if err != nil {
+		fmt.Printf("Could not read from file.\n")
+		return nil
+	}
 
-    block, _ := pem.Decode(pub_read_in)
-    if block == nil {
-        fmt.Printf("Could not decode the read in block.\n")
-        return nil
-    }
+	block, _ := pem.Decode(pub_read_in)
+	if block == nil {
+		fmt.Printf("Could not decode the read in block.\n")
+		return nil
+	}
 
-    pub, err := x509.ParsePKIXPublicKey(block.Bytes)
-    if err != nil {
-        fmt.Printf("Could not Parse pub key")
-        return nil
-    }
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		fmt.Printf("Could not Parse pub key")
+		return nil
+	}
 
-    return pub.(*rsa.PublicKey)
+	return pub.(*rsa.PublicKey)
 }
 
 // Just for LCN paper
 func Perform_moodle_login(w http.ResponseWriter, req *http.Request) bool {
-    _, err := req.Cookie("li")
-    if err != nil {
-        // Transform existing http request into log POST form
-//        req.Method = "POST"
+	_, err := req.Cookie("li")
+	if err != nil {
+		// Transform existing http request into log POST form
+		//        req.Method = "POST"
 
+		// Set cookie presenting that user is logged in
+		fmt.Printf("Performing Moodle log in...\n")
+		li_cookie := &http.Cookie{
+			Name:   "li",
+			Value:  "yes",
+			MaxAge: 36000,
+			Path:   "/",
+		}
+		//req.AddCookie(li_cookie)
+		http.SetCookie(w, li_cookie)
+		return true
+	}
 
-        // Set cookie presenting that user is logged in
-        fmt.Printf("Performing Moodle log in...\n")
-        li_cookie := &http.Cookie{
-            Name: "li",
-            Value: "yes",
-            MaxAge: 36000,
-            Path: "/",
-        }
-        //req.AddCookie(li_cookie)
-        http.SetCookie(w, li_cookie)
-        return true
-    }
-
-    fmt.Printf("Cookie is present, user is logged in\n")
-    return false
+	fmt.Printf("Cookie is present, user is logged in\n")
+	return false
 
 }
 
-func ParseRsaPrivateKeyFromPemStr(privPEMlocation string) (*rsa.PrivateKey) {
-    priv_read_in, err := ioutil.ReadFile(privPEMlocation)
-    if err != nil {
-        fmt.Printf("Could not read from file.\n")
-        return nil
-    }
+func ParseRsaPrivateKeyFromPemStr(privPEMlocation string) *rsa.PrivateKey {
+	priv_read_in, err := ioutil.ReadFile(privPEMlocation)
+	if err != nil {
+		fmt.Printf("Could not read from file.\n")
+		return nil
+	}
 
-    block, _ := pem.Decode(priv_read_in)
-    if block == nil {
-        fmt.Printf("Could not decode the read in block.\n")
-        return nil
-    }
+	block, _ := pem.Decode(priv_read_in)
+	if block == nil {
+		fmt.Printf("Could not decode the read in block.\n")
+		return nil
+	}
 
-    priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-    if err != nil {
-        fmt.Printf("Could not Parse priv key: \n", err)
-        return nil
-    }
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		fmt.Print("Could not Parse priv key: \n", err)
+		return nil
+	}
 
-    return priv.(*rsa.PrivateKey)
+	return priv.(*rsa.PrivateKey)
 }
 
 func handleFormReponse(msg string, w http.ResponseWriter) {
-    form := `<html>
+	form := `<html>
         <body>
         <center>
         <form action="/" method="post">
@@ -228,34 +231,34 @@ func handleFormReponse(msg string, w http.ResponseWriter) {
         </html>
         `
 
-    //fmt.Println(msg)
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    fmt.Fprintf(w, form)
+	//fmt.Println(msg)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, form)
 }
 
 func userIsInLDAP(userName, password string) bool {
-        client := &ldap.LDAPClient{
-                Base:         "ou=people,dc=planetexpress,dc=com",
-                Host:         "10.4.0.52",
-                Port:         389,
-                UseSSL:       false,
-                BindDN:       "cn=admin,dc=planetexpress,dc=com",
-                BindPassword: "GoodNewsEveryone",
-                UserFilter:   "(uid=%s)",
-                GroupFilter: "(memberUid=%s)",
-                Attributes:   []string{"givenName", "sn", "mail", "uid"},
-        }
-        // It is the responsibility of the caller to close the connection
-        defer client.Close()
+	client := &ldap.LDAPClient{
+		Base:         env.Config.Ldap.Base,
+		Host:         env.Config.Ldap.Host,
+		Port:         env.Config.Ldap.Port,
+		UseSSL:       env.Config.Ldap.UseSSL,
+		BindDN:       env.Config.Ldap.BindDN,
+		BindPassword: env.Config.Ldap.BindPassword,
+		UserFilter:   env.Config.Ldap.UserFilter,
+		GroupFilter:  env.Config.Ldap.GroupFilter,
+		Attributes:   env.Config.Ldap.Attributes,
+	}
+	// It is the responsibility of the caller to close the connection
+	defer client.Close()
 
-        ok, _, err := client.Authenticate(userName, password)
-        if err != nil {
-                fmt.Printf("Error authenticating user %s: %+v\n", userName, err)
-                return false
-        }
-        if !ok {
-                fmt.Printf("Authenticating failed for user %s\n", userName)
-                return false
-        }
-        return true
+	ok, _, err := client.Authenticate(userName, password)
+	if err != nil {
+		fmt.Printf("Error authenticating user %s: %+v\n", userName, err)
+		return false
+	}
+	if !ok {
+		fmt.Printf("Authenticating failed for user %s\n", userName)
+		return false
+	}
+	return true
 }
