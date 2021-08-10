7a0ee7dc1b7c1850ce5ccd5bf7df1282779b7f1b
Marie Lohbeck <marie.lohbeck@uni-ulm.de>
2021-08-10 09:59:40 +0200

    removed unused functions

--- proxies/proxies.go
+++ proxies/proxies.go
@@ -3,61 +3,14 @@ package proxies
 import (
 	"crypto/tls"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"time"
 
 	env "local.com/leobrada/ztsfc_http_pep/env"
 )
 
 var (
-	Basic_auth_proxy      *httputil.ReverseProxy
 	Pdp_client_pool       []*http.Client
 	Sfp_logic_client_pool []*http.Client
-	Service_proxy         *httputil.ReverseProxy
 )
-
-func NewServiceProxy() *httputil.ReverseProxy {
-
-	basic_auth_url, _ := url.Parse("https://10.5.0.53")
-	proxy := httputil.NewSingleHostReverseProxy(basic_auth_url)
-
-	// When the PEP is acting as a client; this defines his behavior
-	proxy.Transport = &http.Transport{
-		TLSHandshakeTimeout: 0 * time.Millisecond,
-		TLSClientConfig: &tls.Config{
-			// TODO: Replace it by loading the cert for the first SF in the chain
-			Certificates:           []tls.Certificate{env.Config.Sf_pool["dummy"].X509KeyPair_shown_by_pep_to_sf},
-			InsecureSkipVerify:     true,
-			ClientAuth:             tls.RequireAndVerifyClientCert,
-			ClientCAs:              env.Config.CA_cert_pool_pep_accepts_from_int,
-			SessionTicketsDisabled: false,
-		},
-	}
-
-	return proxy
-}
-
-func NewBasicAuthProxy() *httputil.ReverseProxy {
-
-	basic_auth_url, _ := url.Parse("https://10.4.0.52")
-	proxy := httputil.NewSingleHostReverseProxy(basic_auth_url)
-
-	// When the PEP is acting as a client; this defines his behavior
-	proxy.Transport = &http.Transport{
-		TLSHandshakeTimeout: 0 * time.Millisecond,
-		TLSClientConfig: &tls.Config{
-			// TODO: Replace it by loading the cert for the first SF in the chain
-			Certificates:           []tls.Certificate{env.Config.Sf_pool["dummy"].X509KeyPair_shown_by_pep_to_sf},
-			InsecureSkipVerify:     true,
-			ClientAuth:             tls.RequireAndVerifyClientCert,
-			ClientCAs:              env.Config.CA_cert_pool_pep_accepts_from_int,
-			SessionTicketsDisabled: false,
-		},
-	}
-
-	return proxy
-}
 
 func NewClientPool(poolSize int, certShownByPEP tls.Certificate) []*http.Client {
 	client_pool := make([]*http.Client, poolSize)
